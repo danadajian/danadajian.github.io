39db87b6c5b86de632af50c542dc6c18754d4aed
danadajian <danadajian@gmail.com>
2020-04-13 19:34:54 +0000

    Circle CI build 2020-04-13_19-34-54

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e6412ad7ce0e0699776bc29760e4ca9.js"
+  "/precache-manifest.310a56c89c0fa79a9e2afe0d519ddae5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
