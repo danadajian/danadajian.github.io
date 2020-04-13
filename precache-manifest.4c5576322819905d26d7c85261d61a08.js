602375c860c382578f98569361af6bbd6ff61e3c
danadajian <danadajian@gmail.com>
2020-04-13 13:43:36 -0400

    * trimmed down to build files to serve

---
+++ precache-manifest.4c5576322819905d26d7c85261d61a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e876277cae0390c43a7e56d2fa9dd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13de0d023f441cda636c",
+    "url": "/static/css/main.0a977655.chunk.css"
+  },
+  {
+    "revision": "52af64679a256eba272a",
+    "url": "/static/js/2.868bfcab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.868bfcab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13de0d023f441cda636c",
+    "url": "/static/js/main.2c530109.chunk.js"
+  },
+  {
+    "revision": "48094acfed90137e9f4d",
+    "url": "/static/js/runtime-main.199b66b5.js"
+  },
+  {
+    "revision": "cef0c4439c87743acc0dfc8334d54bee",
+    "url": "/static/media/chicago.cef0c443.jpg"
+  }
+]);
